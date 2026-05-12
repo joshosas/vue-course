00adead3d4712ec4587082cca3547e8a4b719ab5
Joshua <joshuaosaro20@gmail.com>
2026-05-12 19:12:29 +0100

    setup router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <!-- App.vue -->
 
 <script setup>
-import Hero from './components/Hero.vue'
-import Navbar from './components/Navbar.vue'
-import HomeCards from './components/HomeCards.vue'
-import JobListings from './components/JobListings.vue'
+import Navbar from '@/components/Navbar.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <Navbar />
-  <Hero title="E concern U" />
-  <HomeCards />
-  <JobListings :limit="3" , showButton="true" />
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<!-- HomeView.vue -->
+
+<script setup>
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+</script>
+
+<template>
+  <Hero title="E concern U" />
+  <HomeCards />
+  <JobListings :limit="3" , showButton="true" />
+</template>
